56e0aa3ea46aa37d56fa6f8f17d9a1f534f16d23
Caelin Sutch <caelinsutch@gmail.com>
2020-10-19 13:27:08 -0700

    Hooked up joystick to ROS

--- src/molecules/joystick-card.tsx
+++ src/molecules/joystick-card.tsx
@@ -2,12 +2,25 @@ import React from 'react';
 import { Joystick } from 'react-joystick-component';
 import { Center } from '@chakra-ui/core';
 import { IJoystickUpdateEvent } from 'react-joystick-component/build/lib/Joystick';
+import ROSLIB from 'roslib';
 import MainCard from '../atoms/main-card';
+import { useROS } from '../hooks/ros.hook';
+
+let movementTopic: ROSLIB.Topic;
 
 const JoystickCard: React.FC = () => {
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { getTopic, isConnected } = useROS();
+
+  if (isConnected && movementTopic == null) {
+    movementTopic = getTopic('/cmd_vel', 'geometry_msgs/Twist');
+  }
+
   const handleMove = (data: IJoystickUpdateEvent): void => {
-    // console.log(data);
+    const rosTwistMessage: ROSLIB.Message = {
+      linear: { x: data.y, y: 0.0, z: 0.0 },
+      angular: { x: 0.0, y: 0.0, z: data.x },
+    };
+    movementTopic.publish(rosTwistMessage);
   };
 
   return (
